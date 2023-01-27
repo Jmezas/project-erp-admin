9fb49175180f3dec7e35e3b705176b4fa0a4e37d
Jmezas <yaser77334600@gmail.com>
2023-01-26 21:33:16 -0500

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlAPI: "http://localhost:3000",
+  urlAPI: "https://api-prueba-ck9m.onrender.com",
   UrlConsultaDocumento: "http://tinaymar-001-site3.ctempurl.com/api/Consulta",
   IGV: 18, //se puede cambiar el IGV en el futuro por lo que se deja como variable de entorno para que sea facil de cambiar en un futuro
   //variable de inicio
